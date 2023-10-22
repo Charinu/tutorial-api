3153b8b357c60066cda705cfa9ccbb77cfcde8c6
Charin <charin.up@gmail.com>
2023-10-22 18:40:56 +0700

    new-commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "baby-shark-db"
+  HOST: "bmlx3df4ma7r1yh4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  USER: "qjl2ji92swipmw3h",
+  PASSWORD: "xasv60ko2zzmvny7",
+  DB: "y07l4f6k2p5jhog8"
 };
